9fbbcc73c5011c29cd94bcb9f1b8ee2d5b77ea9c
Rahuluuu <rahul.anagani2006@gmail.com>
2025-04-20 17:27:41 +0530

    asdffsda

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import CaptainLogin from './Pages/CaptainLogin.jsx'
+import UserLogin from './Pages/UserLogin.jsx'
+import CaptainSignup from './Pages/CaptainSignup.jsx'
+import UserSignup from './Pages/UserSignup.jsx'
 import { Provider } from 'react-redux'
 import store from './store/index.js'
-
-import Home from "./Pages/Home.jsx"
-import CaptainLogin from "./Pages/CaptainLogin.jsx"
-import UserLogin from "./Pages/UserLogin.jsx"
-import CaptainSignup from "./Pages/CaptainSignup.jsx"
-import UserSignup from "./Pages/UserSignup.jsx"
 import DashBoard from './Pages/DashBoard.jsx'
 import AuthProtect from './Pages/AuthProtect.jsx'
 import UserLogout from './Pages/UserLogout.jsx'
@@ -20,28 +19,78 @@ import CapSignout from './Pages/CapSignout.jsx'
 import CaptainRiding from './Pages/CaptainRiding.jsx'
 import SocketProvider from './Pages/SocketProvider.jsx'
 import UserRiding from './Pages/UserRiding.jsx'
+import Map from './components/Map.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <SocketProvider><App /></SocketProvider>,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: '/login',
+          element: <UserLogin />,
+        },
+        {
+          path: '/signup',
+          element: <UserSignup />,
+        },
+        {
+          path: '/captain-login',
+          element: <CaptainLogin />,
+        },
+        {
+          path: '/captain-signup',
+          element: <CaptainSignup />,
+        },
+        {
+          path: '/captain-riding',
+          element: <CaptainRiding />,
+        },
+        {
+          path: '/dashboard',
+          element: (
+            <AuthProtect>
+              <DashBoard />
+            </AuthProtect>
+          ),
+        },
+        {
+          path: '/user-riding',
+          element: <UserRiding />,
+        },
+        {
+          path: '/logout',
+          element: <UserLogout />,
+        },
+        {
+          path: '/captain-logout',
+          element: <CapSignout />,
+        },
+        {
+          path: '/CapDashboard',
+          element: (
+            <CapProtect>
+              <CapDashBoard />
+            </CapProtect>
+          ),
+        },
+      ],
+    },
+  ],
+  {
+    basename: '/RideHail', // Add this line to handle the base path
+  }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
-        <SocketProvider>
-          <App />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<UserLogin />} />
-            <Route path="/signup" element={<UserSignup />} />
-            <Route path="/captain-login" element={<CaptainLogin />} />
-            <Route path="/captain-signup" element={<CaptainSignup />} />
-            <Route path="/captain-riding" element={<CaptainRiding />} />
-            <Route path="/dashboard" element={<AuthProtect><DashBoard /></AuthProtect>} />
-            <Route path="/user-riding" element={<UserRiding />} />
-            <Route path="/logout" element={<UserLogout />} />
-            <Route path="/captain-logout" element={<CapSignout />} />
-            <Route path="/CapDashboard" element={<CapProtect><CapDashBoard /></CapProtect>} />
-          </Routes>
-        </SocketProvider>
-      </Router>
+      <RouterProvider router={router}></RouterProvider>
     </Provider>
   </StrictMode>
 )
