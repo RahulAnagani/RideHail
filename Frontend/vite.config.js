ebfebddc31a01ec9173040b978a1f86f79953cd1
Rahuluuu <rahul.anagani2006@gmail.com>
2025-04-20 17:12:06 +0530

    adsfa

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 3000,       // Optional: specify a port (default is 5173)
-  },
+  base: '/RideHail/', 
 })
